34b62194ee6e8e53ff9c44151cc56f58e4b6ac7b
PaulBarbeyJunr <paul@junr.studio>
2026-01-02 10:15:06 +0100

    auth jwt (#1)

---
+++ src/main/java/com/chatop/dto/LoginRequest.java
@@ -0,0 +1,3 @@
+package com.chatop.dto;
+
+public record LoginRequest(String login, String password) {}
---
+++ src/main/java/com/chatop/dto/RegisterRequest.java
@@ -0,0 +1,3 @@
+package com.chatop.dto;
+
+public record RegisterRequest(String name, String email, String password) {}
